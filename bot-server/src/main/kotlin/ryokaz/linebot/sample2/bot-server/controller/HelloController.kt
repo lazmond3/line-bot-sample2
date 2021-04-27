1f348eee18313c47fbcf5eda83a480db4c97906c
lazmond3 <moikilo00+xcode@gmail.com>
2021-04-28 01:27:32 +0900

    CHANGE hello controller

--- bot-server/src/main/kotlin/ryokaz/linebot/sample2/bot-server/controller/HelloController.kt
+++ bot-server/src/main/kotlin/ryokaz/linebot/sample2/bot-server/controller/HelloController.kt
@@ -1,10 +1,18 @@
 package ryokaz.linebot.sample2.bot_server.controller
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import ryokaz.linebot.sample2.common.Hello
+
+// まだログ出力されない
+private val logger = KotlinLogging.logger {}
 
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    fun hello() = "hello"
+    fun hello(): Hello {
+        logger.warn { "world in printlin" }
+        return Hello("world")
+    }
 }
