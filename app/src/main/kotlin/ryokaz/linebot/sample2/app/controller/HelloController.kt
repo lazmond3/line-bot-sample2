a1246f4ad6d0b7e5e72ceb98d0c0c1deeeaf6ffe
lazmond3 <moikilo00+xcode@gmail.com>
2021-04-28 09:37:58 +0900

    DELETE controller in bot server and ADD kotlin-logging in app controller

--- app/src/main/kotlin/ryokaz/linebot/sample2/app/controller/HelloController.kt
+++ app/src/main/kotlin/ryokaz/linebot/sample2/app/controller/HelloController.kt
@@ -1,11 +1,17 @@
 package ryokaz.linebot.sample2.app.controller
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import ryokaz.linebot.sample2.common.Hello
 
+private val logger = KotlinLogging.logger {}
+
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    fun hello() = Hello("world")
+    fun hello(): Hello {
+        logger.info { "hello in app controller logger.info" }
+        return Hello("world")
+    }
 }
