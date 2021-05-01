24625da055a473d295b676162a7f33ab36301ff6
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-01 20:53:57 +0900

    [terraform] CHANGE BACKEND CLOUD

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -5,10 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "s3" {
-    bucket = "moikilo00-tfstate-bucket"
-    key    = "line-bot-sample2/dev.tfstate"
-    region = "ap-northeast-1"
+  backend "remote" {
+    organization = "line-bot-sample2"
+    workspaces {
+      name = "line-bot-sample2"
+    }
   }
 }
 
