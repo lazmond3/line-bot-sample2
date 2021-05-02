e8f30e0b26175463f14a1bec48df3375b39c99f3
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-02 10:05:53 +0900

    [terraform] [refactor] vpc の natゲートウェイの設定を別ファイルに移動する。

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -55,29 +55,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-# NAT ゲートウェイ 用 Elastic IP付与 (public に NAT ゲートウェイを接続)
-# resource "aws_eip" "nat" {
-#   count = length(var.public_subnet_cidrs)
-
-#   vpc = true
-
-#   tags = {
-#     Name = "${var.app_name}-natgw-${count.index}"
-#   }
-# }
-
-# # NAT ゲートウェイ
-# resource "aws_nat_gateway" "this" {
-#   count = length(var.public_subnet_cidrs)
-
-#   subnet_id     = element(aws_subnet.publics.*.id, count.index)
-#   allocation_id = element(aws_eip.nat.*.id, count.index) # ここで eip をつないでいる
-
-#   tags = {
-#     Name = "${var.app_name}-${count.index}"
-#   }
-# }
-
 # ルートテーブル
 # Route Table
 # https://www.terraform.io/docs/providers/aws/r/route_table.html
@@ -136,17 +113,6 @@ resource "aws_route_table" "privates" {
     Name = "${var.app_name}-private-${count.index}"
   }
 }
-
-# NATをオフにする
-# resource "aws_route" "privates" {
-#   count = length(var.private_subnet_cidrs)
-
-#   destination_cidr_block = "0.0.0.0/0"
-
-#   route_table_id = element(aws_route_table.privates.*.id, count.index)
-#   nat_gateway_id = element(aws_nat_gateway.this.*.id, count.index)
-# }
-
 resource "aws_route_table_association" "privates" {
   count = length(var.private_subnet_cidrs)
 
