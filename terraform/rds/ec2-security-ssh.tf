766435f194c566d768d95e8c55b1cd63e1ae224c
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-02 09:58:57 +0900

    [terraform] [DEBUG] [WIP] [ec2] private の ec2 にも、セキュリティグループでssh を許可してみる。

--- terraform/rds/ec2-security-ssh.tf
+++ terraform/rds/ec2-security-ssh.tf
@@ -17,6 +17,25 @@ resource "aws_security_group" "ec2_public_bastian" {
   }
 }
 
+resource "aws_security_group" "ec2_private" {
+  name   = "ec2_private"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+}
+
 resource "aws_security_group" "mysql_to_private" {
   name   = "mysql_to_private"
   vpc_id = var.vpc_id
