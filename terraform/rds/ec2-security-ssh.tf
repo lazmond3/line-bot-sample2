bb9fba7ef0989503f19c64109ae162479ed1e839
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-02 14:46:03 +0900

    [terraform] [DEBUG] [WIP] [FIX] [ec2] private からNAT に行かない問題で、ingress 0-65535 も追加してみた。そしてcidr は、ingress: vpc内部しか直接通らないはずなので、10.0.0.0/16 をしてみた。 egress は 0/0 にした。

--- terraform/rds/ec2-security-ssh.tf
+++ terraform/rds/ec2-security-ssh.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "ec2_private_nat_to_global" {
   name   = "ec2_private_nat_to_global"
   vpc_id = var.vpc_id
 
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 65535
