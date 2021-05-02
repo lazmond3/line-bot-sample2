a9cfa10e73b78a60634fce6df0802eae9dcfea06
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-02 14:14:00 +0900

    [terraform] [DEBUG] [WIP] [FIX] [ec2] [another-vpc] 別VPC egress icmp を全て許可 for all

--- terraform/rds/debug-ec2-vpc.tf
+++ terraform/rds/debug-ec2-vpc.tf
@@ -136,6 +136,13 @@ resource "aws_security_group" "main" {
   }
 
   egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"] # 本当は 0.0.0.0 で設定してみてもいいけど
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
