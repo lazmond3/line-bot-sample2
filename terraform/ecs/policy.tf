0eb85cc00a98f662718c7a240d2f6ab3a1f81e98
lazmond3 <moikilo00+xcode@gmail.com>
2021-05-08 18:46:57 +0900

    [terraform] ecs policy cloudwatch logs

--- terraform/ecs/policy.tf
+++ terraform/ecs/policy.tf
@@ -53,3 +53,30 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_for_ec
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+# 謎に ECSがコンテナが死んでしまう理由がわからないので、cloudwatch にアクセスする
+resource "aws_iam_role_policy" "cloudwatch_logs" {
+  name = "cloudwatch_logs"
+  role = aws_iam_role.ecs_task_execution_role.name
+
+  policy = <<-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "logs:CreateLogGroup",
+                  "logs:CreateLogStream",
+                  "logs:DescribeLogStreams",
+                  "logs:PutLogEvents"
+              ],
+              "Resource": [
+                  "arn:aws:logs:ap-northeast-1:554506578892:log-group:*"
+              ]
+          }
+      ]    
+  }
+  EOF
+}
