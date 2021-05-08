86f597af3e149bf792f07f90419fd082aa230232
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-09 01:50:49 +0900

    [terraform] [ecs-rds] [debug] [ec2] ec2 の個数を減らす

--- terraform/rds/debug-ec2-security-ssh.tf
+++ terraform/rds/debug-ec2-security-ssh.tf
@@ -1,112 +1,112 @@
-# resource "aws_security_group" "ec2_public_bastian" {
-#   name   = "ec2_bastian"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "ec2_public_bastian" {
+  name   = "ec2_bastian"
+  vpc_id = var.vpc_id
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "tcp"
-#     cidr_blocks = [var.vpc_cidr]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+}
 
-# resource "aws_security_group" "ec2_private" {
-#   name   = "ec2_private"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "ec2_private" {
+  name   = "ec2_private"
+  vpc_id = var.vpc_id
 
-#   # ingress {
-#   #   from_port   = 22
-#   #   to_port     = 22
-#   #   protocol    = "tcp"
-#   #   cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
-#   # }
+  # ingress {
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+  # }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "tcp"
-#     cidr_blocks = [var.vpc_cidr]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+}
 
-# resource "aws_security_group" "ec2_private_nat_to_global" {
-#   name   = "ec2_private_nat_to_global"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "ec2_private_nat_to_global" {
+  name   = "ec2_private_nat_to_global"
+  vpc_id = var.vpc_id
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["10.0.0.0/16"]
-#   }
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_security_group" "mysql_to_private" {
-#   name   = "mysql_to_private"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "mysql_to_private" {
+  name   = "mysql_to_private"
+  vpc_id = var.vpc_id
 
-#   egress {
-#     from_port   = 3306
-#     to_port     = 3306
-#     protocol    = "tcp"
-#     cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
-#   }
-# }
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+  }
+}
 
-# # ====================
-# #
-# # Security Group
-# #
-# # ====================
-# resource "aws_security_group" "example" {
-#   vpc_id = var.vpc_id
-#   name   = "example"
+# ====================
+#
+# Security Group
+#
+# ====================
+resource "aws_security_group" "example" {
+  vpc_id = var.vpc_id
+  name   = "example"
 
-#   tags = {
-#     Name = "example"
-#   }
-# }
+  tags = {
+    Name = "example"
+  }
+}
 
-# # インバウンドルール(ssh接続用)
-# resource "aws_security_group_rule" "in_ssh" {
-#   security_group_id = aws_security_group.example.id
-#   type              = "ingress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-# }
+# インバウンドルール(ssh接続用)
+resource "aws_security_group_rule" "in_ssh" {
+  security_group_id = aws_security_group.example.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+}
 
-# # インバウンドルール(pingコマンド用)
-# resource "aws_security_group_rule" "in_icmp" {
-#   security_group_id = aws_security_group.example.id
-#   type              = "ingress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   from_port         = -1
-#   to_port           = -1
-#   protocol          = "icmp"
-# }
+# インバウンドルール(pingコマンド用)
+resource "aws_security_group_rule" "in_icmp" {
+  security_group_id = aws_security_group.example.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+}
 
-# # アウトバウンドルール(全開放)
-# resource "aws_security_group_rule" "out_all" {
-#   security_group_id = aws_security_group.example.id
-#   type              = "egress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-# }
+# アウトバウンドルール(全開放)
+resource "aws_security_group_rule" "out_all" {
+  security_group_id = aws_security_group.example.id
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+}
