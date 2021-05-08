90af47e3284e9470a2aa3687525619ec262c499b
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-09 07:50:58 +0900

    [terraform] [ecs-rds] [format][refactor] ファイルを整理 security group を rds_security.tf に移行

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -15,32 +15,6 @@ locals {
   name = "linebot-sample2-rds-mysql"
 }
 
-resource "aws_security_group" "this" {
-  name        = local.name
-  description = local.name
-
-  # vpc_id = aws_vpc.main.id
-  vpc_id = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  tags = {
-    Name = "${local.name}"
-  }
-}
-
 resource "aws_db_subnet_group" "this" {
   name        = local.name
   description = local.name
---
+++ terraform/rds/rds_security.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "this" {
+  name        = local.name
+  description = local.name
+
+  # vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  tags = {
+    Name = "${local.name}"
+  }
+}
