c79103e3ebcfe4ad65e2a827ccda569916d3cd29
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-08 22:03:21 +0900

    [terraform] cloudwatch 用のVPCエンドポイントを作成

--- terraform/ecr/vpc_endpoint.tf
+++ terraform/ecr/vpc_endpoint.tf
@@ -45,3 +45,13 @@ resource "aws_vpc_endpoint" "ssm" {
   security_group_ids  = [aws_security_group.vpc_endpoint.id]
   private_dns_enabled = true
 }
+
+# ECS から cloudwatch にアクセスするために必要なVPCエンドポイント
+resource "aws_vpc_endpoint" "cloudwatch" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.monitoring"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = var.aws_subnet_private_ids
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+}
