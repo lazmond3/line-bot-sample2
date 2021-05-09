251e4be2dca2541fa3d60b3e8bf9822e6c86e8a9
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-09 09:24:58 +0900

    [java] kotlin println settings DB info 環境変数情報のprint

--- app/src/main/kotlin/ryokaz/linebot/sample2/app/AppApplication.kt
+++ app/src/main/kotlin/ryokaz/linebot/sample2/app/AppApplication.kt
@@ -7,5 +7,14 @@ import org.springframework.boot.runApplication
 class AppApplication
 
 fun main(args: Array<String>) {
+    println("DB_ADDRESS: ${System.getenv("DB_ADDRESS")}")
+    println("DB_PORT: ${System.getenv("DB_PORT")}")
+    println("DB_USER: ${System.getenv("DB_USER")}")
+    println("DB_DATABASE: ${System.getenv("DB_DATABASE")}")
+    val pass = System.getenv("DB_PASSWORD")?.let {
+        "${it.subSequence(0,4)}-length:${it.length}"
+    }
+        ?: "null"
+    println("DB_PASSWORD: ${pass}")
     runApplication<AppApplication>(*args)
 }
