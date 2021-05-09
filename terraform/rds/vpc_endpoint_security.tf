0f974fc6b3a8687e8500edabc48faf4fb5e26312
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-10 00:32:56 +0900

    [terraform] [rds-ecs] rds の vpc endpoint の security について、 cidr 0.0.0.0 に更新

--- terraform/rds/vpc_endpoint_security.tf
+++ terraform/rds/vpc_endpoint_security.tf
@@ -6,14 +6,16 @@ resource "aws_security_group" "vpc_endpoint_rds" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+    # cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
+    cidr_blocks = ["0.0.0.0/0"] # 本当は 0.0.0.0 で設定してみてもいいけど
   }
 
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    # cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"] # 本当は 0.0.0.0 で設定してみてもいいけど
   }
 }
 
