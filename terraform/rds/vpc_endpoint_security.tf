d54ec65c41cc640695d08342ca28176eca5682e0
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-09 22:00:28 +0900

    [terraform] FMT

--- terraform/rds/vpc_endpoint_security.tf
+++ terraform/rds/vpc_endpoint_security.tf
@@ -23,9 +23,9 @@ resource "aws_security_group" "vpc_endpoint_rds2" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "-1"
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "-1"
     # cidr_blocks = [var.vpc_cidr] # 本当は 0.0.0.0 で設定してみてもいいけど
     cidr_blocks = ["0.0.0.0/0"] # 本当は 0.0.0.0 で設定してみてもいいけど
   }
