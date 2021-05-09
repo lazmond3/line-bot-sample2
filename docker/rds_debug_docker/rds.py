ab3366cede331d7133d447014f5fe5114d36e82e
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-09 21:57:40 +0900

    [docker] [debug] [terraform] [ecs-rds] [debug] update rds.py for PORT, endpoint など、仕様確認

--- docker/rds_debug_docker/rds.py
+++ docker/rds_debug_docker/rds.py
@@ -3,9 +3,10 @@ import os
 Database_endpoint = os.environ['ENDPOINT']
 Username = os.environ['USER']
 Password = os.environ['PASS']
+PORT = int(os.environ['PORT'])
 try:
     print("Connecting to "+Database_endpoint)
-    db = pymysql.connect(Database_endpoint,Username,Password)
+    db = pymysql.connect(host=Database_endpoint,user=Username,password=Password, port=PORT)
     print ("Connection successful to "+Database_endpoint)
 except Exception as e:
     print ("Connection unsuccessful due to "+str(e))
