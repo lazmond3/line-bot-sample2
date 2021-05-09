e2a3641214e22766e1bb5a7ff581e037de5fd806
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-10 08:56:54 +0900

    [terraform] [rds-ecs] update python をもう一回試す。pythonコードに dev.auto.tfvars を修正

--- docker/rds_debug_docker/rds.py
+++ docker/rds_debug_docker/rds.py
@@ -8,5 +8,14 @@ try:
     print("Connecting to "+Database_endpoint)
     db = pymysql.connect(host=Database_endpoint,user=Username,password=Password, port=PORT)
     print ("Connection successful to "+Database_endpoint)
+    with db:
+        with db.cursor() as cursor:
+            # Create a new record
+            sql = "SELECT 1+1"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print("result: ", result)
+
+
 except Exception as e:
-    print ("Connection unsuccessful due to "+str(e))
+    print ("FAILED: Connection unsuccessful due to "+str(e))
