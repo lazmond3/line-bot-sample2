042a63304c0e4b40c43497b5cc5436aede78b478
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-10 09:00:27 +0900

    [terraform] [rds-ecs] [ecs] python2 用のロググループ作成して変更

--- terraform/ecs/cloudwatch_resource.tf
+++ terraform/ecs/cloudwatch_resource.tf
@@ -4,4 +4,15 @@ resource "aws_cloudwatch_log_group" "line-bot-sample2" {
   tags = {
     Name = "${var.app_name}"
   }
+
+}
+
+# デバッグ用 python2
+resource "aws_cloudwatch_log_group" "python2" {
+  name = "/aws/ecs/${var.app_name}/python2"
+  # name = "/aws/ecs/python2"
+
+  tags = {
+    Name = "linebot2-python2"
+  }
 }
--- terraform/ecs/data.tf
+++ terraform/ecs/data.tf
@@ -11,6 +11,8 @@ data "template_file" "container_definitions" {
     mysql_user        = var.mysql_user
     db_address        = var.db_address
     db_port           = var.db_port
-    log_group         = "/aws/ecs/${var.app_name}"
+    # log_group         = "/aws/ecs/${var.app_name}"
+    # log_group         = "/aws/ecs/${var.app_name}/python2"
+    log_group = aws_cloudwatch_log_group.python2.name
   }
 }
