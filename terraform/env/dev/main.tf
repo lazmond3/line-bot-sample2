ace00492bf4606094711757a865b62e5adb7d62a
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-13 20:25:07 +0900

    [terraform-refactor] [vpc] main.tf で vpc, alb のみ残して他は削除

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -16,18 +16,13 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-module "ecr-app" {
-  source                          = "../../ecr"
-  ecr-name                        = "${var.ecr_name2}-${var.project_name_app}"
-  vpc_id                          = module.vpc.vpc_id
-  aws_subnet_private_ids          = module.vpc.aws_subnet_private_ids
-  vpc_cidr                        = module.vpc.vpc_cidr
-  aws_route_table_ids_for_private = module.vpc.aws_route_table_ids_for_private
-}
-
-# module "ecr-bot-server" {
-#   source   = "../../ecr"
-#   ecr-name = "${var.ecr_name2}-${var.project_name_bot_server}"
+# module "ecr-app" {
+#   source                          = "../../ecr"
+#   ecr-name                        = "${var.ecr_name2}-${var.project_name_app}"
+#   vpc_id                          = module.vpc.vpc_id
+#   aws_subnet_private_ids          = module.vpc.aws_subnet_private_ids
+#   vpc_cidr                        = module.vpc.vpc_cidr
+#   aws_route_table_ids_for_private = module.vpc.aws_route_table_ids_for_private
 # }
 
 module "route53" {
@@ -43,33 +38,33 @@ module "vpc" {
   app_name = var.app_name
 }
 
-module "alb" {
-  source            = "../../alb"
-  aws_lb_public_ids = module.vpc.aws_subnet_public_ids
-  app_name          = var.app_name
-  vpc_id            = module.vpc.vpc_id
-  cert-arn          = module.route53.cert-arn
-  ecs-id            = module.ecs.ecs-id
-}
+# module "alb" {
+#   source            = "../../alb"
+#   aws_lb_public_ids = module.vpc.aws_subnet_public_ids
+#   app_name          = var.app_name
+#   vpc_id            = module.vpc.vpc_id
+#   cert-arn          = module.route53.cert-arn
+#   ecs-id            = module.ecs.ecs-id
+# }
 
-module "ecs" {
-  source                                         = "../../ecs"
-  app_name                                       = var.app_name
-  vpc_id                                         = module.vpc.vpc_id
-  ecs_load_balancer_target_arn                   = module.alb.aws_lb_target_group_main_arn
-  ecs_subnets                                    = module.vpc.aws_subnet_private_ids
-  template_file_path                             = var.template_file_path
-  container_repository                           = var.container_repository
-  container_tag                                  = var.container_tag
-  container_name                                 = var.container_name
-  container_port                                 = var.container_port
-  aws_ecr_repository_name                        = module.ecr-app.aws_ecr_repository_name
-  aws_ssm_parameter_database_password_secret_arn = module.rds.aws_ssm_parameter_database_password_secret_arn
-  mysql_database                                 = var.mysql_database
-  mysql_user                                     = var.mysql_user
-  db_address                                     = module.rds.db_address
-  db_port                                        = module.rds.db_port
-}
+# module "ecs" {
+#   source                                         = "../../ecs"
+#   app_name                                       = var.app_name
+#   vpc_id                                         = module.vpc.vpc_id
+#   ecs_load_balancer_target_arn                   = module.alb.aws_lb_target_group_main_arn
+#   ecs_subnets                                    = module.vpc.aws_subnet_private_ids
+#   template_file_path                             = var.template_file_path
+#   container_repository                           = var.container_repository
+#   container_tag                                  = var.container_tag
+#   container_name                                 = var.container_name
+#   container_port                                 = var.container_port
+#   aws_ecr_repository_name                        = module.ecr-app.aws_ecr_repository_name
+#   aws_ssm_parameter_database_password_secret_arn = module.rds.aws_ssm_parameter_database_password_secret_arn
+#   mysql_database                                 = var.mysql_database
+#   mysql_user                                     = var.mysql_user
+#   db_address                                     = module.rds.db_address
+#   db_port                                        = module.rds.db_port
+# }
 
 # rds をやってみる
 module "rds" {
--- terraform/env/dev/variables.tf
+++ terraform/env/dev/variables.tf
@@ -50,3 +50,20 @@ variable "mysql_user" {
 variable "mysql_password" {
   type = string
 }
+
+# vpc 用の variables
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "vpc_azs" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+}
+
+variable "vpc_public_subnet_cidrs" {
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_private_subnet_cidrs" {
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+}
